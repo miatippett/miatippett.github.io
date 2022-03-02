c7bca1f5266af8739d1f81d45f9cc9a804268004
miatippett <mtippett@eastsideprep.org>
2022-03-02 13:15:23 -0800

    commit

--- portfolio_final/js/script.js
+++ portfolio_final/js/script.js
@@ -3,3 +3,21 @@ function menuOnClick() {
     document.getElementById("nav").classList.toggle("change");
     document.getElementById("menu-bg").classList.toggle("change-bg");
 }
+
+// top button
+var mybutton = document.getElementById("myBtn");
+
+window.onscroll = function() { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
